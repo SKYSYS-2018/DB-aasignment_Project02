645f819e73d13386c10b5fbe7d1e9dec4b8dc0a2
Sachintha <ravindusachintha53@gmail.com>
2018-06-16 03:17:02 +0530

    update repo

--- pages/courses.php
+++ pages/courses.php
@@ -5,6 +5,7 @@
 	<title>Courses Managing</title>
 	<link rel="stylesheet" type="text/css" href="../css/main.css">
 	<link rel="stylesheet" type="text/css" href="../css/nav.css">
+	<link rel="stylesheet" type="text/css" href="../css/subpage.css">
 </head>
 
 <body>
@@ -28,10 +29,38 @@
 			<a href="./professors.php" class="nav-item text-center"> PROFESSORS </a>
 			<a href="./company_sessions.php" class="nav-item text-center" > COMPANY SESSIONS </a>
 			<a href="./books.php" class="nav-item text-center"> BOOKS </a>
-			<a href="./lab_sessions.php" class="nav-item text-center"> LAB SESSIONS </a>
+			<a href="./lab_sessions.php" class="nav-item-rightmost text-center"> LAB SESSIONS </a>
 		</div>
 
-		
+		<!-- Sub Page -->
+		<div class="container sub-page border-default">
+
+			<div class="sub-page-box">
+				<div class="container sub-page-title text-center border-default"> COURSES </div>
+			</div>
+
+			<div class="sub-page-box">
+				<button class="sub-page-btn border-default"> Add a New Course </button>
+			</div>
+
+			<div class="sub-page-box">
+				<table class="container sub-page-table text-center">
+					<tr>
+						<th>Column 1</th>
+						<th>Column 2</th>
+						<th>Column 3</th>
+						<th>Column 4</th>
+					</tr>
+					<tr>
+						<td>data 1</td>
+						<td>data 2</td>
+						<td>data 3</td>
+						<td>data 4</td>
+					</tr>
+				</table>
+			</div>
+
+		</div>
 
 	</div>
 
