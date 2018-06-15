bed4200940cb425b11d86670215708334f7e491e
Sachintha <ravindusachintha53@gmail.com>
2018-06-16 01:39:51 +0530

    create php pages and css files

---
+++ pages/books.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Books Managing</title>
+	<link rel="stylesheet" type="text/css" href="../css/main.css">
+	<link rel="stylesheet" type="text/css" href="../css/nav.css">
+</head>
+
+<body>
+
+	<div class="container page border-default">
+
+		<!-- Title Box -->
+		<div class="container title-box border-default">
+			<a href="../index.php"><div class="title left">UNIVERSITY SYSTEM</div></a>
+			<div class="logview-box right">
+				<div class="logview-label text-center"> LOGGED AS ADMIN </div>
+				<div class="logview-btn text-center"> LOG OUT </div>
+			</div>
+		</div>
+
+		<!-- Navigation Panel -->
+		<div class="container nav-panel border-default">
+			<a href="./departments.php" class="nav-item-leftmost text-center"> DEPARTMENTS </a>
+			<a href="./students.php" class="nav-item text-center"> STUDENTS </a>
+			<a href="./courses.php" class="nav-item text-center"> COURSES </a>
+			<a href="./professors.php" class="nav-item text-center"> PROFESSORS </a>
+			<a href="./company_sessions.php" class="nav-item text-center" > COMPANY SESSIONS </a>
+			<a href="./books.php" class="nav-item text-center"> BOOKS </a>
+			<a href="./lab_sessions.php" class="nav-item text-center"> LAB SESSIONS </a>
+		</div>
+
+		
+
+	</div>
+
+</body>
+
+</html>
