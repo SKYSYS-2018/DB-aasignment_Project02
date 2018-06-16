318efd62b5143f1c194366ab1a945b55c94eadbd
ManpowerTracking <asithaindrajithk9@gmail.com>
2018-06-16 23:35:11 +0530

    asitha update

---
+++ js/add_book.js
@@ -0,0 +1,6 @@
+function show_add_book(){
+    document.getElementById("add_book").style.display="block";
+}
+function hide_add_book(){
+    document.getElementById("add_book").style.display="none";
+}
